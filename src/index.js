2df2857624d169b57db4f666cd60baccd64d4a0c
Artefall <32461472+Artefall@users.noreply.github.com>
2018-07-08 21:37:34 +0300

    v1.1

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ function getAjaxObject(link){
 }
 //1
 getAjaxObject('http://www.mocky.io/v2/55f7485935681951044b3dc6')
-    .then(getAjaxObject('http://www.mocky.io/v2/55f748b33568195d044b3dc8'))
-    .then(getAjaxObject('http://www.mocky.io/v2/55f748ca3568195f044b3dc9'))
+    .then(() => getAjaxObject('http://www.mocky.io/v2/55f748b33568195d044b3dc8'))
+    .then(() => getAjaxObject('http://www.mocky.io/v2/55f748ca3568195f044b3dc9'))
     .catch(function(error){
         console.log('Провал:')
         console.error(error);
